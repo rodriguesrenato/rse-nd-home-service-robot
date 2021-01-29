d28a9f00360e41791603f1f3acad61dd2095e0ab
Renato Rodrigues <renato.rodrigues.rfr@gmail.com>
2021-01-28 20:41:27 -0800

    refacor: script adjustments

--- scripts/pick_objects.sh
+++ scripts/pick_objects.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-xterm  -e  " source /opt/ros/kinetic/setup.bash; source ~/catkin_ws/devel/setup.bash; roslaunch rse_nd_home_service_robot world.launch" &
+xterm  -e  " source /opt/ros/kinetic/setup.bash; source ~/catkin_ws/devel/setup.bash; roslaunch renato_robot world.launch" &
 sleep 5
-xterm  -e  " source /opt/ros/kinetic/setup.bash; source ~/catkin_ws/devel/setup.bash; roslaunch rse_nd_home_service_robot amcl.launch" &
+xterm  -e  " source /opt/ros/kinetic/setup.bash; source ~/catkin_ws/devel/setup.bash; roslaunch renato_robot amcl.launch" &
 sleep 3
-xterm  -e  " source /opt/ros/kinetic/setup.bash; source ~/catkin_ws/devel/setup.bash; roslaunch rse_nd_home_service_robot view_navigation.launch" & 
+xterm  -e  " source /opt/ros/kinetic/setup.bash; source ~/catkin_ws/devel/setup.bash; roslaunch renato_robot view_navigation.launch" & 
 sleep 5
-xterm  -e  " source /opt/ros/kinetic/setup.bash; source ~/catkin_ws/devel/setup.bash; rosrun pick_objects pick_objects" & 
+xterm  -e  " source /opt/ros/kinetic/setup.bash; source ~/catkin_ws/devel/setup.bash; rosrun pick_objects pick_objects" &
