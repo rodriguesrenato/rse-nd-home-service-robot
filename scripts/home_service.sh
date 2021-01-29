0ea121b17e1ceea8b5a9bf6618fa5b410e1e925f
Renato Rodrigues <renato.rodrigues.rfr@gmail.com>
2021-01-28 20:42:00 -0800

    feat: added a full home service robot script

---
+++ scripts/home_service.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+xterm  -e  " source /opt/ros/kinetic/setup.bash; source ~/catkin_ws/devel/setup.bash; roslaunch renato_robot world.launch" &
+sleep 5
+xterm  -e  " source /opt/ros/kinetic/setup.bash; source ~/catkin_ws/devel/setup.bash; roslaunch renato_robot amcl.launch" &
+sleep 3
+xterm  -e  " source /opt/ros/kinetic/setup.bash; source ~/catkin_ws/devel/setup.bash; roslaunch renato_robot view_navigation.launch" & 
+sleep 5
+xterm  -e  " source /opt/ros/kinetic/setup.bash; source ~/catkin_ws/devel/setup.bash; rosrun add_markers add_markers" & 
+sleep 5
+xterm  -e  " source /opt/ros/kinetic/setup.bash; source ~/catkin_ws/devel/setup.bash; rosrun pick_objects pick_objects" & 
