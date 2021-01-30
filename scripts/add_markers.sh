4ead642c1693862cf412d933a3132fea0b894606
Renato Rodrigues <renato.rodrigues.rfr@gmail.com>
2021-01-30 10:29:22 -0800

    refactor: adjust scripts to meet project rubric

--- scripts/add_markers.sh
+++ scripts/add_markers.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+xterm  -e  " source /opt/ros/kinetic/setup.bash; source ~/catkin_ws/devel/setup.bash; roslaunch renato_robot world.launch" &
+sleep 5
+xterm  -e  " source /opt/ros/kinetic/setup.bash; source ~/catkin_ws/devel/setup.bash; roslaunch renato_robot amcl.launch" &
+sleep 3
+xterm  -e  " source /opt/ros/kinetic/setup.bash; source ~/catkin_ws/devel/setup.bash; roslaunch renato_robot view_navigation.launch" & 
+sleep 5
+xterm  -e  " source /opt/ros/kinetic/setup.bash; source ~/catkin_ws/devel/setup.bash; rosrun add_markers add_markers" & 
+sleep 5
 xterm  -e  " source /opt/ros/kinetic/setup.bash; source ~/catkin_ws/devel/setup.bash; rosservice call /job_request \"job: 'Add'
 pose:
   position:
--- scripts/home_service.sh
+++ scripts/home_service.sh
@@ -2,7 +2,7 @@
 xterm  -e  " source /opt/ros/kinetic/setup.bash; source ~/catkin_ws/devel/setup.bash; roslaunch renato_robot world.launch" &
 sleep 5
 xterm  -e  " source /opt/ros/kinetic/setup.bash; source ~/catkin_ws/devel/setup.bash; roslaunch renato_robot amcl.launch" &
-sleep 3
+sleep 5
 xterm  -e  " source /opt/ros/kinetic/setup.bash; source ~/catkin_ws/devel/setup.bash; roslaunch renato_robot view_navigation.launch" & 
 sleep 5
 xterm  -e  " source /opt/ros/kinetic/setup.bash; source ~/catkin_ws/devel/setup.bash; rosrun add_markers add_markers" & 
